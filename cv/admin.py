c74b0502fbd7a7e4a74175d89267e36531cc5321
qrames <ramque@orange.fr>
2018-12-12 11:55:19 +0100

    install django-colorful and create model Category (model / model admin)

--- cv/admin.py
+++ cv/admin.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
-from .models import Technologie, Work
+from .models import Technologie, Work, Category
 
 
 class TechnologieAdmin(admin.ModelAdmin):
@@ -15,5 +15,10 @@ class WorkAdmin(admin.ModelAdmin):
     model = Work
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Technologie, TechnologieAdmin)
 admin.site.register(Work, WorkAdmin)
--- cv/models.py
+++ cv/models.py
@@ -2,23 +2,31 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from colorful.fields import RGBColorField
 # Create your models here.
 
-class Work(models.Model):
-  name = models.CharField(max_length=50)
-  image = models.ImageField(upload_to='works')
-  url = models.URLField(null=True, blank=True)
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    color = RGBColorField()
 
-  def __unicode__(self):
-      return self.name
+    def __unicode__(self):
+        return self.name
+
+
+class Work(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='works')
+    url = models.URLField(null=True, blank=True)
+
+    def __unicode__(self):
+        return self.name
 
 
 class Technologie(models.Model):
-  name = models.CharField(max_length=50)
-  icon = models.CharField(max_length=50)
-  progress = models.PositiveSmallIntegerField()
-  url = models.URLField(null=True, blank=True)
-
-  def __unicode__(self):
-      return self.name
+    name = models.CharField(max_length=50)
+    icon = models.CharField(max_length=50)
+    progress = models.PositiveSmallIntegerField()
+    url = models.URLField(null=True, blank=True)
+    category = models.ForeignKey(Category)
+    def __unicode__(self):
+        return self.name
